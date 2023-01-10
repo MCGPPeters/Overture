c76bc23540ad48a4147777cedbb165fef26c649d
Maurice CGP Peters <mcgppeters@gmail.com>
2023-01-10 12:34:56 +0100

    Exponential backoff strategy

---
+++ src/Overture/Backoff.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overture;
+
+/// <summary>
+/// 
+/// </summary>
+public static class BackOff
+{
+    public static TimeSpan[] Exponentially(int maxNumberOfTimes = 3) => Enumerable
+        .Range(1, maxNumberOfTimes)
+        .Select(
+            x =>
+            {
+                Random rnd = new();
+                TimeSpan seconds = TimeSpan.FromSeconds(System.Math.Pow(2, x));
+                // add random number of milliseconds to decrease the chance globally synchronized retries
+                TimeSpan milliseconds = TimeSpan.FromMilliseconds(rnd.Next());
+
+                return seconds.Add(milliseconds);
+
+            }).ToArray();
+}
